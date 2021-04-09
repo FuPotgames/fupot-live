46a720899085347ed41b951d82aeaffd835d542a
tahmidc <tahmidahmed1000@gmail.com>
2021-04-08 23:28:31 -0400

    timer is working now

--- frontend/src/app/user-answer/user-answer.page.ts
+++ frontend/src/app/user-answer/user-answer.page.ts
@@ -14,6 +14,8 @@ export class UserAnswerPage implements OnInit {
   current_timestamp: number;
   interval:any;
 
+  timer:string;
+
   constructor(
     private navController: NavController,
     private activatedRoute:ActivatedRoute,
@@ -24,54 +26,15 @@ export class UserAnswerPage implements OnInit {
     this.activatedRoute.queryParams.subscribe(async (res)=>{
       this.question = res;
     });
-    this.getTimeStamp();
+    //this.getTimeStamp();
   }
 
-  ngOnDestroy(){
-    clearInterval(this.interval);
-  }
+  
 
   answer() {
     this.navController.navigateRoot(['/user-submit']);
   }
-
-  async getTimeStamp(){
-    this.userQuestionService.getTimestamp().subscribe((res)=>{
-      this.current_timestamp = Number(res[0].slice(19));
-      this.countdown(this.current_timestamp,String(this.question.ends_at_original));
-    });
-  }
-
-  countdown(curr_time,end_time){
-    let endTime = new Date(end_time).getTime();
-    let current_time = new Date(moment.unix(curr_time).toISOString());
-      // Update the count down every 1 second
-      this.interval = setInterval(function () {
-        // Get todays date and time
-        current_time.setSeconds(current_time.getSeconds() + 1);
-        let NOW = current_time.getTime();
-        // Find the distance between now and the count down date
-        let distance = endTime - NOW;
-        // Time calculations for days, hours, minutes and seconds
-        let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-        
   
-        
-  
-        // If the count down is over, write some text 
-        if (distance < 0) {
-          clearInterval(this.interval);
-        }
-        else{
-          // Output the result in an element with id="demo"
-        console.log(days + "d " + hours + "h "
-          + minutes + "m " + seconds + "s ");
-        }
-      }, 1000);
-    }
 
   
 
--- frontend/src/app/user-available-questions/user-available-questions.page.ts
+++ frontend/src/app/user-available-questions/user-available-questions.page.ts
@@ -11,11 +11,16 @@ import * as moment from 'moment';
 })
 export class UserAvailableQuestionsPage implements OnInit {
   questions = new Array();
+  question_timers = new Array();
   colors = ['fupot_blue','fupot_purple','fupot_pink','fupot_teal'];
   card_colors = ['blueCard','purpleCard','pinkCard','tealCard'];
   group_id: any;
   current_timestamp: number;
 
+  interval:any;
+  question: any;
+
+  timer = 'something';
   
 
   constructor(
@@ -24,29 +29,37 @@ export class UserAvailableQuestionsPage implements OnInit {
     private navController:NavController
   ) { }
 
-  ngOnInit() {
-    this.getTimeStamp();
+  async ngOnInit() {
+    
     this.activatedRoute.queryParams.subscribe(async (res)=>{
-      console.log(res)
       this.group_id = res.group_id
+           
       
     });
-    this.getGroupQuestions();
+   this.current_timestamp = Number((await this.userQuestionService.getTimestamp().toPromise())[0].slice(19));
+    console.log(this.current_timestamp)
+    await this.getGroupQuestions();
+   
+
+ 
+   
+    
+    
+    
   }
 
   // gets the groups questions specified by the group_id
   // as well as gets the question data from owner-question page from the route
   async getGroupQuestions() {
-    this.userQuestionService.getGroupQuestions(this.group_id).subscribe(async (res) => {
-      var temp = res;
+      var temp = await this.userQuestionService.getGroupQuestions(this.group_id).toPromise();
       var count = 0;
       for (var x = 0; x < temp.length; x++) {
+        this.countdown(this.current_timestamp,String(temp[x]['ends_at_original']),x);
         temp[x]['color'] = this.colors[count];
         temp[x]['card_color'] = this.card_colors[count];
         if((this.isAvailable(temp[x].starts_at_original,temp[x].ends_at_original,this.current_timestamp) == true)){
           //Question Available
             temp[x]['available'] = true;
-          
         }
         else{
           temp[x]['available'] = false;
@@ -59,11 +72,8 @@ export class UserAvailableQuestionsPage implements OnInit {
         }
       }
       temp = null;
-      console.log(this.questions)
+      
 
-
-    });
-  
   
 }
 
@@ -78,14 +88,58 @@ isAvailable(start_date,end_date,current_timestamp) {
   }
 }
 
-getTimeStamp(){
-  this.userQuestionService.getTimestamp().subscribe((res)=>{
-    this.current_timestamp = Number(res[0].slice(19));
-  });
-}
 
 user_answer_page(i){
   this.navController.navigateForward('/user-answer',{'queryParams': this.questions[i]});
 }
+
+ngOnDestroy(){
+    clearInterval(this.interval);
+  
+  
+}
+
+// Timer starts counting back to the end_time
+countdown(curr_time,end_time,i){
+    let endTime = new Date(end_time).getTime();
+    let current_time = new Date(moment.unix(curr_time).toISOString());
+
+    console.log(current_time)
+
+      // Update the count down every 1 second
+      this.interval = setInterval(function () {
+        // Get todays date and time
+        current_time.setSeconds(current_time.getSeconds() + 1);
+        let NOW = current_time.getTime();
+        // Find the distance between now and the count down date
+        let distance = endTime - NOW;
+        // Time calculations for days, hours, minutes and seconds
+        let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        
+        
+        
+  
+        // If the count down is over, write some text 
+        if (distance < 0) {
+          console.log("test")
+          clearInterval(this.interval);
+        }
+        else{
+          // Output the result in an element with id="demo"
+          this.timer = days + "d " + hours + "h "
+          + minutes + "m " + seconds + "s ";
+
+            document.getElementById(i).innerHTML = "Time Left: "+hours + "h:"+ minutes + "m:" + seconds + "s ";   
+          
+          
+        }
+        
+      }, 1000);
+      
+  
+  }
 
 }
